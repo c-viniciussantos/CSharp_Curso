2a9446a5594c6467d3d448d6d6f9636b39719d18
C. Vinicius Santos <c.viniciussantos@outlook.com>
2021-03-16 15:38:01 -0300

    Tipos primitivos

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
-namespace ConsoleApp1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Ola Mundo!");
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+
+            byte n1 = 126;
+            int n2 = 2147483647;
+            long n3 = 2147483648L;
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
         }
     }
 }
